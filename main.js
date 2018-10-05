520ae051d395cb9aaa50dc62164e0c13bc0e989f
Mel <zzidante@gmail.com>
2018-10-05 00:30:25 -0700

    Allow for elements to be erased. Add board state.

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
+const $_pristineBoardState = {
+  zero: 0,
+  one: 1,
+  two: 2,
+
+  three: 3,
+  four: 4,
+  five: 5,
+
+  six: 6,
+  seven: 7,
+  eight: 8,
+}
+
 let $_currentPlayer = 'X'
 let $_openingState = true;
+let $_boardState
 
 function displayOpeningBanner(openingState) {
   const helloMessage = document.getElementById("introduction");
+  const gameSquares = document.getElementsByClassName("game-square");
   
   if (openingState) {
     helloMessage.innerText = ' ' 
@@ -10,6 +26,10 @@ function displayOpeningBanner(openingState) {
     setTimeout(() => { helloMessage.classList.toggle('disappear'); }, 501);
     $_openingState = false;
   } else {
+    for(i = 0; i < gameSquares.length; i++) {
+      gameSquares[i].innerText = ' '
+    } // make this a method
+
     helloMessage.classList.remove("disappear")
     helloMessage.classList.toggle('fade');   
     helloMessage.innerText = 'Can you beat the Matrix?' 
