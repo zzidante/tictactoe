1d8b556be8772d243fb229d6a80729e01e23b718
Mel <zzidante@gmail.com>
2018-10-12 23:59:54 -0700

    refactor playGame(): change dependency siblings to children

--- src/actions.js
+++ src/actions.js
@@ -3,43 +3,42 @@ const DOM = getDomComponent;
 const $_ = globals;
 
 // game play
-function playGame() { renderState($_.openingState) };
-
-function renderState(state) {
-  if (state) {
-    startScreenState()
-  } else {
-    restart()
-  };
-}
-
-function startScreenState() {
+function playGame() { 
   const helloMessage = DOM({name: 'introduction', getType: 'id'});
-
-  helloMessage.innerText = ' '
-  helloMessage.classList.toggle('fade');
-  setTimeout(() => { helloMessage.classList.toggle('disappear'); }, 501);
-  $_.openingState = false;
-}
-
-function restart() {
-  const helloMessage = DOM({name: 'introduction', getType: 'id'});
-
-  helloMessage.classList.remove("disappear")
-  helloMessage.classList.toggle('fade');
-  helloMessage.innerText = 'Can you beat the Matrix?'
-  $_.openingState = true;
-  clearBoard();
-}
-
-function clearBoard() {
   const gameSquares = DOM({name: 'game-square', getType: 'class'});
 
-  for(i = 0; i < gameSquares.length; i++) {
-      gameSquares[i].innerText = ' '
-      gameSquares[i].classList.remove("player-o");
-      gameSquares[i].classList.remove("player-x");
+  this.startScreenState = () => {
+    helloMessage.innerText = ' '
+    helloMessage.classList.toggle('fade');
+    setTimeout(() => { helloMessage.classList.toggle('disappear'); }, 501);
+    $_.openingState = false;
   }
+
+  this.clearBoard = () => {
+    for(i = 0; i < gameSquares.length; i++) {
+        gameSquares[i].innerText = ' '
+        gameSquares[i].classList.remove("player-o");
+        gameSquares[i].classList.remove("player-x");
+    }
+  };
+
+  this.restart = () => {
+    helloMessage.classList.remove("disappear")
+    helloMessage.classList.toggle('fade');
+    helloMessage.innerText = 'Can you beat the Matrix?'
+    $_.openingState = true;
+    this.clearBoard();
+  }
+
+  this.renderState = (state) => {
+    if (state) {
+      this.startScreenState()
+    } else {
+      this.restart()
+    };
+  }
+  
+  this.renderState($_.openingState) 
 };
 
 function takeTurn(event, elementName) {
